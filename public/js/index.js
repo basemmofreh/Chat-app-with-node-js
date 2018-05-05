f6629d84f8ff3f58b7f1e6738987c38cf1b2da9e
Bassem <AnError@live.com>
2018-05-05 14:51:48 -0800

    add newMessage and createMessage events 2

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+  var socket = io();
+  var emailForm = document.getElementById('emailForm');
+
+socket.on('connect',function(){
+  console.log('im connected to server');
+
+emailForm.addEventListener('submit',function(e){
+e.preventDefault();
+  var data = {
+    email:document.getElementById('emailInput').value,
+    text:document.getElementById('textInput').value
+  }
+
+  socket.emit('createEmail',data)
+
+
+  socket.on('newEmail',function(data){
+    document.querySelector('.textMsg').innerHTML= 'From ' +data.email + ' Text : ' +data.text +' AT :'+' <p>'+data.time+ '</p>';
+
+  })
+})
+
+});
+
+socket.on('disconnect',function(){
+  console.log("server is down");
+});
