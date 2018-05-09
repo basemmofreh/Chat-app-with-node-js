b41f66e5768c302d0ee495aa68ddc561186ce6c2
Bassem <AnError@live.com>
2018-05-08 17:59:05 -0800

    add form for messages and show incoming msgs in brower

--- public/js/index.js
+++ public/js/index.js
@@ -3,31 +3,35 @@
 
 socket.on('connect',function(){
   console.log('im connected to server');
-
-emailForm.addEventListener('submit',function(e){
-e.preventDefault();
-  var data = {
-    email:document.getElementById('emailInput').value,
-    text:document.getElementById('textInput').value
-  }
-  socket.on('newEmail',function(data){
-    document.querySelector('.textMsg').innerHTML= 'From ' +data.email + ' Text : ' +data.text +' AT :'+' <p>'+data.time+ '</p>';
-  })
 })
 
-
 socket.on('newMessage',function(data){
-alert(JSON.stringify(data));
+$('#chat').append('<li>'+'From : '+data.from+' msg: '+data.text+'</li>')
 console.log(JSON.stringify(data));
 })
 
 socket.on('welcomeMessage',function(msg){
   document.getElementsByTagName('h1')[0].innerHTML+=msg;
 });
-
-
-});
-
 socket.on('disconnect',function(){
   console.log("server is down");
+});
+
+
+$('#emailForm').on('submit',function(e){
+e.preventDefault();
+  var data = {
+    email:document.getElementById('emailInput').value,
+    text:document.getElementById('textInput').value
+  }
+  // socket.on('createMessage',function(data){
+  //   document.querySelector('.textMsg').innerHTML= 'From ' +data.email + ' Text : ' +data.text +' AT :'+' <p>'+data.time+ '</p>';
+  // })
+  socket.emit('createMessage',{
+    from:$('#emailInput').val(),
+    text:$('#textInput').val()
+  },function(){
+    console.log('Sent at : ',new Date().toString());
+  });
+
 });
--- server/server.js
+++ server/server.js
@@ -12,19 +12,17 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 io.on('connection',(socket)=>{
-    socket.emit('welcomeMessage','Welcome To the chat Application');
-  socket.broadcast.emit('newMessage','new User signed in');
+    socket.emit('newMessage',generateMsg('Admin','Welcome To the chat Application'));
+  socket.broadcast.emit('newMessage',generateMsg('Admin:','new User signed in'));
 
-    socket.on('createMessage',(message)=>{
+    socket.on('createMessage',(message,callback)=>{
       console.log('createMessage',message);
-
-
-
       //socket.emit only for single connection who asked for
       //io.emit send for all connections
       //socket.broadcast.emit for all connections except for the one submitted
+  io.emit('newMessage',generateMsg(message.from,message.text))
 
-    io.emit('newMessage',generateMsg('admin','welcome to the chat app'));
+    callback();
   });
 
     socket.on('disconnect',()=>{
